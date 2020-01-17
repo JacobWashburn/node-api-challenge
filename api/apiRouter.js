1c7c72271d540d96d99a19e6f88bda3d17e48908
Jacob Washburn <jacobrwashburn@gmail.com>
2020-01-17 09:20:17 -0700

    some changes from copy

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,7 +1,5 @@
 const express = require('express');
 
-const Shouts = require('../shouts/shouts-model.js');
-
 const router = express.Router();
 
 router.use(express.json());
@@ -10,7 +8,7 @@ router.get('/', (req, res) => {
   res.status(200).json({ api: 'up' });
 });
 
-router.get('/shouts', (req, res, next) => {
+router.get('/', (req, res, next) => {
   Shouts.find()
     .then(shouts => {
       res.status(200).json(shouts);
@@ -18,7 +16,7 @@ router.get('/shouts', (req, res, next) => {
     .catch(error => next(error));
 });
 
-router.post('/shouts', (req, res, next) => {
+router.post('/', (req, res, next) => {
   Shouts.add(req.body)
     .then(shout => {
       res.status(201).json(shout);
@@ -29,9 +27,7 @@ router.post('/shouts', (req, res, next) => {
 router.use(errorHandler);
 
 function errorHandler(error, req, res, next) {
-  // do something with error before responding
-  // like saving it to a database, sending a mail to the admin
-  // or using an external logging service
+
   res.status(500).json(error.message);
 }
 
